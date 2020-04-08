7cad961030de54312cbe7b9435650780ab49e018
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2020-04-08 14:21:40 +0700

    added corrected version

---
+++ app/src/main/java/edu/pam/tugas_rancang/BudgetManagementActivity.java
@@ -0,0 +1,14 @@
+package edu.pam.tugas_rancang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BudgetManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget_management);
+    }
+}
---
+++ app/src/main/java/edu/pam/tugas_rancang/logic/Trip.java
@@ -0,0 +1,68 @@
+package edu.pam.tugas_rancang.logic;
+
+import androidx.annotation.Nullable;
+
+public class Trip {
+    private int id;
+    private int tour_id; // from class Tour
+    private String name;
+    private String description = "";
+
+    public Trip(int id, int tour_id, String name) {
+        this.id = id;
+        this.tour_id = tour_id;
+        this.name = name;
+    }
+
+    public Trip(int id, int tour_id, String name, @Nullable String description) {
+        this.id = id;
+        this.tour_id = tour_id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Trip(int id, Tour tour, String name) {
+        this.id = id;
+        this.tour_id = tour.getId();
+        this.name = name;
+    }
+
+    public Trip(int id, Tour tour, String name, @Nullable String description) {
+        this.id = id;
+        this.tour_id = tour.getId();
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTour_id() {
+        return tour_id;
+    }
+
+    public void setTour_id(int tour_id) {
+        this.tour_id = tour_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
